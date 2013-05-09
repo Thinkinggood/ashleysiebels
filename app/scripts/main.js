eefbb7d08675e7fe3a8cb173f2ae15cdc823aba8
Matthew Hayes <matt@mysterycommand.com>
2013-05-08 21:00:23 -0500

    Focus enabled!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(function() {
     'use strict';
 
     $('.portfolio-piece')
-        .on('click touchend', function(event) {
+        .on('click touchend focus', function(event) {
             event.preventDefault();
             event.stopPropagation();
 
@@ -18,13 +18,48 @@ $(function() {
                     .height($this.find('figure').height())
                     .siblings()
                     .removeClass('active')
-                    .height(90);
+                    .height(90)
+                    .find('figcaption')
+                    .removeClass('selected');
+            }
+            $this.find('.more-or-less').text('+');
+            $this.find('figcaption').removeClass('selected');
+        });
+
+    $('.portfolio-piece-more')
+        .on('click touchend focus', function(event) {
+            var $this = $(this);
+            var $piece = $this.closest('.portfolio-piece');
+            if ( ! $piece.hasClass('active')) {
+                if (event.type === 'focus') {
+                    $piece.focus();
+                }
+                return;
+            }
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            var $caption = $piece.find('figcaption');
+
+            if ($caption.hasClass('selected')) {
+                $this.find('.more-or-less').text('+');
+                $caption
+                    .removeClass('selected');
+            } else {
+                $this.find('.more-or-less').text('-');
+                $caption
+                    .addClass('selected');
             }
         });
 
-    $(document).on('click', function() {
+
+    $(document).on('click touchend focus', function() {
         $('.active')
             .removeClass('active')
             .height(90);
+
+        $('.selected').removeClass('selected');
+        $('.more-or-less').text('+');
     });
 });
