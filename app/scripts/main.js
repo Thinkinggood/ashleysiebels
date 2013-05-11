2c68ef2fb7050b3d4109c8d125a6beb0eccabef6
Matthew Hayes <matt@mysterycommand.com>
2013-05-11 12:55:48 -0500

    Updated some styles and behaviors.
    Put text in sentence case and up-cased it via CSS. Fixed the
    double-click/focus event problem.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,59 +2,100 @@ $(function() {
     'use strict';
 
     $('.portfolio-piece')
-        .on('click focus', function(event) {
+        .on('click', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+        })
+        .on('focusin', function(event) {
             event.preventDefault();
             event.stopPropagation();
 
             var $this = $(this);
+            $this
+                .addClass('active')
+                .height($this.find('figure').height())
+                .siblings()
+                .removeClass('active')
+                .height(90)
+                .find('figcaption')
+                .removeClass('selected');
 
-            if ($this.hasClass('active')) {
-                $this
-                    .removeClass('active')
-                    .height(90);
-            } else {
-                $this
-                    .addClass('active')
-                    .height($this.find('figure').height())
-                    .siblings()
-                    .removeClass('active')
-                    .height(90)
-                    .find('figcaption')
-                    .removeClass('selected');
-            }
             $this.find('.more-or-less').text('+');
             $this.find('figcaption').removeClass('selected');
+        })
+        .on('focusout', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            // var $this = $(this);
+            // if ($this.hasClass('active')) {
+            //     $this
+            //         .removeClass('active')
+            //         .height(90)
+            //         .find('figcaption')
+            //         .removeClass('selected');
+            // }
         });
 
     $('.portfolio-piece-more')
-        .on('click focus', function(event) {
-            var $this = $(this);
-            var $piece = $this.closest('.portfolio-piece');
-            if ( ! $piece.hasClass('active')) {
-                if (event.type === 'focus') {
-                    $piece.focus();
-                }
-                return;
-            }
-
+        .on('click', function(event) {
             event.preventDefault();
             event.stopPropagation();
 
+            var $this = $(this);
+            var $piece = $this.closest('.portfolio-piece');
             var $caption = $piece.find('figcaption');
 
             if ($caption.hasClass('selected')) {
-                $this.find('.more-or-less').text('+');
+                $this
+                    .find('.more-or-less')
+                    .text('+');
+
                 $caption
                     .removeClass('selected');
+
             } else {
-                $this.find('.more-or-less').text('-');
+                $this
+                    .find('.more-or-less')
+                    .text('-');
+
                 $caption
                     .addClass('selected');
             }
+        })
+        .on('focusin', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            var $this = $(this);
+            var $piece = $this.closest('.portfolio-piece');
+
+            if ( ! $piece.hasClass('active') && event.type === 'focusin') {
+                $this.blur();
+                $piece.focus();
+                return;
+            }
+
+            $this.click();
+        })
+        .on('focusout', function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            var $this = $(this);
+            var $piece = $this.closest('.portfolio-piece');
+            var $caption = $piece.find('figcaption');
+
+            $this
+                .find('.more-or-less')
+                .text('+');
+
+            $caption
+                .removeClass('selected');
         });
 
 
-    $(document).on('click focus', function() {
+    $(document).on('click', function() {
         $('.active')
             .removeClass('active')
             .height(90);
